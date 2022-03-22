f75e0f3206e7e7d23d1db81ea93a146319e351e3
alba moreno <alba.moreno95@gmail.com>
2022-03-22 18:29:39 +0100

    definition of header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
 // styles
 import './App.scss';
 
+// components
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-      App component! 👋🏻
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            {/* Logo (link to /home) */}
+            {/* Breadcrumb with active page and links */}
+            {/* Trolley icon with # of products added */}
+        </div>
+    )
+}
+
+export default Header
